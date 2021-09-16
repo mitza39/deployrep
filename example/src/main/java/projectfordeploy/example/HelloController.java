0b6b932a5b8693a091b11f425f59b466ddb2b129
mitza39 <33368747+mitza39@users.noreply.github.com>
2021-09-16 14:14:39 +0300

    Update HelloController.java

--- example/src/main/java/projectfordeploy/example/HelloController.java
+++ example/src/main/java/projectfordeploy/example/HelloController.java
@@ -1,24 +1,15 @@
 package projectfordeploy.example;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
  
-  @RequestMapping("/hello")
-  public String hello() {
-    StringBuilder message = new StringBuilder("Hello Google App Engine!");
-    try {
-      InetAddress ip = InetAddress.getLocalHost();
-      message.append(" From host: " + ip);
-    } catch (UnknownHostException e) {
-      e.printStackTrace();
-    }
-      return message.toString();
-    }
+	  @GetMapping("/deploy")
+	    public String welcome(){
+	        return "This app is deployed on Cloud";
+	    
+	}
  
 }
